29485c38cf7a3cb6293a8c5000bd91f99a6ed227
TylerDStephens <112495458+TylerDStephens@users.noreply.github.com>
2022-08-31 21:21:49 -0500

    Delete UnrealRisk directory

--- UnrealRisk/Source/UnrealRisk/UnrealRisk.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealRisk.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealRisk, "UnrealRisk" );
